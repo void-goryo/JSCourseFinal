e795af4b7ff0b01ca19cfdb71b93fafcf5ee2e0e
Gabe <GABEHJ2001@GMAIL.COM>
2022-01-23 12:23:20 -0800

    bugged out for now

---
+++ formChallenge/JS/main.js
@@ -0,0 +1,25 @@
+/*function validate() {
+    var num = document.getElementById('phone').value;
+    if()
+}*/  //also a great guess and you were almost there. Here is a better way to do it
+
+function validatePhone() {
+    //let phn = document.forms['valphone']['num'].value;
+    let phn = parseInt(document.forms[valphone]['num'].value);
+    var text;
+    let length = pnh.length;
+    // a new varaible = document from the HTML form. first value is the form name, second value is the input name. and .value of course, gets the value
+    if (isNaN(phn)){
+        //if phn is Not a Number (NaN), and the length is equal to 10
+        text = 'Please enter a 10 digit phone number';
+    }
+    else if (length != 10){
+        text = 'Please enter a 10 digit phone number'
+    }
+    else {
+        text = 'Submited!';
+    }
+    console.log(phn);
+    console.log(legnth);
+    document.getElementById('result').innerHTML = validatePhone();
+}
